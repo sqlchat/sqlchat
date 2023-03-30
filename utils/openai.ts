04ab812070c4d64cb72d75cbb4886e8149caf925
boojack <stevenlgtm@gmail.com>
2023-03-30 15:05:13 +0800

    chore: add openai api endpoint config field (#11)

--- utils/openai.ts
+++ utils/openai.ts
@@ -1,6 +1,10 @@
 import { encode } from "@nem035/gpt-3-encoder";
 
+// openAIApiKey is the API key for OpenAI API.
 export const openAIApiKey = process.env.OPENAI_API_KEY;
+
+// openAIApiEndpoint is the API endpoint for OpenAI API. Defaults to https://api.openai.com.
+export const openAIApiEndpoint = process.env.OPENAI_API_ENDPOINT || "https://api.openai.com";
 
 export const countTextTokens = (text: string) => {
   return encode(text).length;
