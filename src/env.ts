1562a61e5e13d8ee667a574431a1437c62b04ca2
steven <stevenlgtm@gmail.com>
2023-04-25 11:43:32 +0800

    chore: update api key name

--- src/env.ts
+++ src/env.ts
@@ -1,2 +1,2 @@
 // API_KEY is using to limit those authorized to use the API and protect the API endpoint.
-export const API_KEY = process.env.API_KEY || "";
+export const API_KEY = process.env.NEXT_PUBLIC_API_KEY || "";
