bf37d766bc3f6b05462599d73abc8956938cf86c
Tianzhou Chen <t@bytebase.com>
2023-05-01 14:35:25 +0800

    chore: add env var to ProcessEnv

---
+++ process.d.ts
@@ -0,0 +1,13 @@
+declare namespace NodeJS {
+  export interface ProcessEnv {
+    // Required. Do not share your OpenAI API key with anyone! It should remain a secret.
+    OPENAI_API_KEY: string;
+    // Optional. OpenAI API endpoint. Defaults to https://api.openai.com.
+    OPENAI_API_ENDPOINT: string;
+    // Optional. Database connection string to store the usage data.
+    USAGE_DATABASE_URL: string;
+    // Optional. API key to protect the backend API endpoint.
+    // This needs to be exposed to the frontend and must be prefixed with NEXT_PUBLIC_.
+    NEXT_PUBLIC_API_KEY: string;
+  }
+}
