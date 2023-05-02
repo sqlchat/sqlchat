145e3bfa73d211209ecddc338a368a076e345e8c
Tianzhou Chen <t@bytebase.com>
2023-05-02 20:22:50 +0800

    chore: remove avatar

---
+++ prisma/migrations/20230502122236_remove_avatar/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `avatar` on the `principal` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "principal" DROP COLUMN "avatar";
