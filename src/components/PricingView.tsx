c005c45f4d68730d425097bb1bf278a612e3e95a
Tianzhou Chen <t@bytebase.com>
2023-05-03 23:31:38 +0800

    chore: add login to buy

--- src/components/PricingView.tsx
+++ src/components/PricingView.tsx
@@ -1,6 +1,5 @@
-import { useSession } from "next-auth/react";
+import { signIn, useSession } from "next-auth/react";
 import { useTranslation } from "react-i18next";
-import { CheckIcon } from "@heroicons/react/20/solid";
 
 import getStripe from "../utils/get-stripejs";
 import { fetchPostJSON } from "../utils/api-helpers";
@@ -40,65 +39,20 @@ const PricingView = () => {
   const { data: session, status } = useSession();
 
   return (
-    <div className="mx-auto max-w-2xl lg:mx-0 lg:flex lg:max-w-none">
-      <div className="p-8 sm:p-10 lg:flex-auto">
-        <h3 className="text-2xl font-bold tracking-tight text-gray-900">
-          Lifetime membership
-        </h3>
-        <p className="mt-6 text-base leading-7 text-gray-600">
-          Lorem ipsum dolor sit amet consect etur adipisicing elit. Itaque amet
-          indis perferendis blanditiis repellendus etur quidem assumenda.
-        </p>
-        <div className="mt-10 flex items-center gap-x-4">
-          <h4 className="flex-none text-sm font-semibold leading-6 text-indigo-600">
-            What’s included
-          </h4>
-          <div className="h-px flex-auto bg-gray-100" />
-        </div>
-        <ul
-          role="list"
-          className="mt-8 grid grid-cols-1 gap-4 text-sm leading-6 text-gray-600 sm:grid-cols-2 sm:gap-6"
-        >
-          {includedFeatures.map((feature) => (
-            <li key={feature} className="flex gap-x-3">
-              <CheckIcon
-                className="h-6 w-5 flex-none text-indigo-600"
-                aria-hidden="true"
-              />
-              {feature}
-            </li>
-          ))}
-        </ul>
-      </div>
-
-      <div className="-mt-2 p-2 lg:mt-0 lg:w-full lg:max-w-md lg:flex-shrink-0">
-        <div className="rounded-2xl bg-gray-50 py-4 text-center ring-1 ring-inset ring-gray-900/5 lg:flex lg:flex-col lg:justify-center">
-          <div className="mx-auto max-w-xs">
-            {session?.user && (
-              <div className="mx-auto max-w-xs px-8">
-                <p className="text-base font-semibold text-gray-600">
-                  Pay once, own it forever
-                </p>
-                <p className="mt-6 flex items-baseline justify-center gap-x-2">
-                  <span className="text-5xl font-bold tracking-tight text-gray-900">
-                    $349
-                  </span>
-                  <span className="text-sm font-semibold leading-6 tracking-wide text-gray-600">
-                    USD
-                  </span>
-                </p>
-                <button
-                  className="mt-10 block w-full rounded-md bg-indigo-600 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                  onClick={() => checkout()}
-                >
-                  Get access
-                </button>
-                <p className="mt-6 text-xs leading-5 text-gray-600">
-                  Invoices and receipts available for easy company reimbursement
-                </p>
-              </div>
-            )}
-          </div>
+    <div className="bg-white">
+      <div className="mx-auto max-w-7xl p-6 lg:flex lg:items-center lg:justify-between lg:px-8">
+        <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+          {t("setting.pricing.early-bird-plan")}
+        </h2>
+        <div className="mt-10 flex items-center gap-x-6 lg:mt-0 lg:flex-shrink-0">
+          <button
+            className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-xl font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            onClick={() => (session?.user?.email ? checkout() : signIn())}
+          >
+            {session?.user?.email
+              ? t("setting.pricing.early-bird-checkout")
+              : t("payment.login-to-buy")}
+          </button>
         </div>
       </div>
     </div>
