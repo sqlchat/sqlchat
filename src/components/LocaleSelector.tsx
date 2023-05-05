a80b0d71ac14d1736a527d23bd6789e9a4abede2
Tianzhou Chen <t@bytebase.com>
2023-05-05 22:58:14 +0800

    chore: only keep english, chinese simplified and spanish
    We are iterating very fast, just don’t have bandwith to handle many languages at this point

--- src/components/LocaleSelector.tsx
+++ src/components/LocaleSelector.tsx
@@ -17,24 +17,8 @@ const localeItemList: LocaleItem[] = [
     label: "简体中文",
   },
   {
-    value: "zhHant",
-    label: "繁體中文",
-  },
-  {
     value: "es",
     label: "Español",
-  },
-  {
-    value: "jp",
-    label: "日本語",
-  },
-  {
-    value: "de",
-    label: "Deutsch",
-  },
-  {
-    value: "th",
-    label: "ยาสุฟุมิ",
   },
 ];
 
--- src/locales/i18n.ts
+++ src/locales/i18n.ts
@@ -2,11 +2,7 @@ import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import enLocale from "./en.json";
 import zhLocale from "./zh.json";
-import zhHant from "./zh-Hant.json";
 import esLocale from "./es.json";
-import jpLocale from "./jp.json";
-import deLocale from "./de.json";
-import thLocale from "./th.json";
 
 i18n.use(initReactI18next).init({
   resources: {
@@ -16,20 +12,8 @@ i18n.use(initReactI18next).init({
     zh: {
       translation: zhLocale,
     },
-    zhHant: {
-      translation: zhHant,
-    },
     es: {
       translation: esLocale,
-    },
-    jp: {
-      translation: jpLocale,
-    },
-    de: {
-      translation: deLocale,
-    },
-    th: {
-      translation: thLocale,
     },
   },
   fallbackLng: "en",
