df6c3a1c2a699fb1621a584a908b778daf09c0e6
CorrectRoadH <a778917369@gmail.com>
2023-05-09 20:53:59 +0800

    implement multiple select scroll

--- src/components/kit/MultipleSelect.tsx
+++ src/components/kit/MultipleSelect.tsx
@@ -18,8 +18,6 @@ interface Props<T = any> {
 
 const MultipleSelect = (props: Props) => {
   const { itemList, value, placeholder, className, onValueChange } = props;
-  console.log(value);
-  console.log(itemList);
   itemList.map((item) => {
     console.log(value.find((v: string) => v == item.value));
   });
@@ -30,7 +28,7 @@ const MultipleSelect = (props: Props) => {
           className || ""
         } flex flex-row justify-between items-center text-sm whitespace-nowrap dark:text-gray-300 bg-white dark:bg-zinc-700 border dark:border-zinc-800 px-3 py-2 rounded-lg`}
       >
-        {value.length === 0 ? placeholder : "多个被选中"}
+        {value.length === 0 ? placeholder : value.join(",")}
         <SelectUI.Icon className="ml-1 w-5 h-auto shrink-0">
           <Icon.BiChevronDown className="w-full h-auto opacity-60" />
         </SelectUI.Icon>
@@ -41,7 +39,7 @@ const MultipleSelect = (props: Props) => {
         leaveFrom="opacity-100"
         leaveTo="opacity-0"
       >
-        <Listbox.Options className="absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+        <Listbox.Options className="absolute mt-1 max-h-60 overflow-y-auto scrollbar-hide w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
           {itemList.map((item) => (
             <Listbox.Option
               className="w-full px-3 py-2 whitespace-nowrap truncate text-ellipsis overflow-x-hidden text-sm rounded-lg flex flex-row justify-between items-center cursor-pointer hover:bg-gray-100 dark:hover:bg-zinc-800"
@@ -50,7 +48,7 @@ const MultipleSelect = (props: Props) => {
             >
               {item.label}
               {(value.find((v: string) => v === item.value) ? true : false) ? (
-                <span className=" inset-y-0 left-0 flex items-center pl-3 text-amber-600">
+                <span className=" inset-y-0 left-0 flex items-center pl-3 text-black">
                   <CheckIcon className="h-5 w-5" aria-hidden="true" />
                 </span>
               ) : null}
