a5907186a2395066d1d9330406991dd8b8142d80
CorrectRoadH <a778917369@gmail.com>
2023-05-11 10:28:07 +0800

    implement all select and empty

--- src/components/ConnectionSidebar.tsx
+++ src/components/ConnectionSidebar.tsx
@@ -203,7 +203,17 @@ const ConnectionSidebar = () => {
                         <div className="flex">
                           <button
                             className="whitespace-nowrap rounded w-full bg-indigo-600 px-2 py-1 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                            onClick={() => {}}
+                            onClick={() => {
+                              if (
+                                conversationStore.getConversationById(
+                                  conversationStore.currentConversationId
+                                )?.tableName?.length == 0
+                              ) {
+                                handleAllSelect();
+                              } else {
+                                handleEmptySelect();
+                              }
+                            }}
                           >
                             {t("connection.select-all-or-empty")}
                           </button>
