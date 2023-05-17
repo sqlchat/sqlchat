88f6e409f377514e54b2fb7a0671f327b58ab276
Tianzhou Chen <t@bytebase.com>
2023-05-18 00:27:47 +0800

    feat: adjust migration

--- prisma/migrations/20230516161429_add_email_to_subscription_to_denormalize/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE "subscription" ADD COLUMN     "email" TEXT NOT NULL DEFAULT '';
-
--- CreateIndex
-CREATE INDEX "subscription_email_idx" ON "subscription"("email");
--- prisma/migrations/20230517134012_subscription_receipt_amount/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE "subscription" ADD COLUMN     "amount" INTEGER NOT NULL DEFAULT 0,
-ADD COLUMN     "currency" TEXT NOT NULL DEFAULT '',
-ADD COLUMN     "description" TEXT NOT NULL DEFAULT '',
-ADD COLUMN     "receipt" TEXT NOT NULL DEFAULT '';
