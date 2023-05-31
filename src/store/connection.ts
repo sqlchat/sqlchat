504655ed2af0bc9466cd72dcdc69002c3f9f5534
tianzhou <t@bytebase.com>
2023-05-31 18:54:23 +0800

    chore: use connection pooler for sample database

--- src/store/connection.ts
+++ src/store/connection.ts
@@ -15,10 +15,10 @@ const samplePGConnection: Connection = {
   id: "sample-pg",
   title: "Sample PostgreSQL",
   engineType: Engine.PostgreSQL,
-  host: "ep-throbbing-thunder-042250.us-west-2.aws.neon.tech",
+  host: "ep-throbbing-thunder-042250-pooler.us-west-2.aws.neon.tech",
   port: "5432",
-  username: "sqlchat",
-  password: "h8yik1pvTQIs",
+  username: "sqlchat_readonly",
+  password: "U5rI8tJMiKWp",
   database: "sample-employee",
 };
 
