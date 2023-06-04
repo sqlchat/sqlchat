051f97ec5379afa114baf39cd21b33354d6a71c2
CorrectRoadH <a778917369@gmail.com>
2023-06-04 15:24:58 +0800

    fix typo

--- src/utils/openai.ts
+++ src/utils/openai.ts
@@ -20,10 +20,12 @@ export function generateDbPromptFromContext(
   userPrompt?: string
 ): string {
   let schema = "";
-  // userPrompt is message that user want to send to bot. When cat prompt in drawer, userPrompt is undefined.
+  // userPrompt is the message that user want to send to bot. When to look prompt in drawer, userPrompt is undefined.
   let tokens = countTextTokens(userPrompt || "");
 
   // Empty table name(such as []) denote all table. [] and `undefined` both are false in `if`
+  // The above comment is out of date. [] is true in `if` now. And no selected table should not denote all table now.
+  // Because in have Token custom number in connectionSidebar. If [] denote all table. the Token will be inconsistent.
   const tableList: string[] = [];
   const selectedSchema = schemaList.find((schema: Schema) => schema.name == (selectedSchemaName || ""));
   if (selectedTablesName) {
