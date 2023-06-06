13606ab9df03928a14db62f83df5c2967a7d6208
CorrectRoadH <a778917369@gmail.com>
2023-06-06 17:09:37 +0800

    chore: delete unnecessary console.log (#126)
    * deleted unused code
    
    * Update index.tsx

--- src/components/ConversationView/index.tsx
+++ src/components/ConversationView/index.tsx
@@ -42,7 +42,7 @@ const ConversationView = () => {
     : [];
   const lastMessage = last(messageList);
   const [showSchemaDrawer, setShowSchemaDrawer] = useState<boolean>(false);
-  console.log(showHeaderShadow);
+  
   useEffect(() => {
     messageStore.messageList.map((message: Message) => {
       if (message.status === "LOADING") {
