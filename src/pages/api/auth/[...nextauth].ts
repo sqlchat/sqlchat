4051f7273ef60f53219e568e14274b6e4c981554
lushenle <lushenle@gmail.com>
2024-02-07 11:14:06 +0800

    set the outgoing mail `noreply@sqlchat.ai` as the default if not specified anything

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -18,7 +18,7 @@ export const authOptions: NextAuthOptions = {
     ? [
         EmailProvider({
           server: process.env.EMAIL_SERVER,
-          from: process.env.EMAIL_FROM,
+          from: process.env.EMAIL_FROM || "noreply@sqlchat.ai",
           // maxAge: 24 * 60 * 60, // How long email links are valid for (default 24h)
         }),
         GoogleProvider({
